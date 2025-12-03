0cd7dd253aea74990fa42ed76361c8c1ede45c1f
artme <artme@imby.energy>
2025-12-03 04:17:19 +0300

    updated promt v1.3.2

--- tasks/eq_image.py
+++ tasks/eq_image.py
@@ -1,10 +1,15 @@
 import os
+import cv2
 from PIL import Image, ImageChops
+from skimage.metrics import structural_similarity
+
+
+
 
 __pathBase = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")), "static")
 __path = os.path.join(__pathBase, "report")
 
-def create_image(clear_path: str, dirty_path: str)-> str:
+def create_image_old(clear_path: str, dirty_path: str)-> str:
     clear_image = Image.open(clear_path)
     dirty_image = Image.open(dirty_path)
 
@@ -19,6 +24,37 @@ def create_image(clear_path: str, dirty_path: str)-> str:
     return os.path.basename(path)
 
 
+
+def create_image(clear_path: str, dirty_path: str) -> str:
+
+    before = cv2.imread(clear_path)
+    after = cv2.imread(dirty_path)
+    path = os.path.join(__path, os.path.basename(dirty_path))
+
+    before_gray = cv2.cvtColor(before, cv2.COLOR_BGR2GRAY)
+    after_gray = cv2.cvtColor(after, cv2.COLOR_BGR2GRAY)
+
+    (score, diff) = structural_similarity(before_gray, after_gray, full=True)
+    diff = (diff * 255).astype("uint8")
+
+    thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area > 55:
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(after, (x, y), (x + w, y + h), (36, 255, 12), 2)
+
+    #cv2.imshow('after', after)
+    cv2.imwrite(path, after)
+    #cv2.waitKey()
+    return path
+
+
+
+
 if __name__ == '__main__':
     import os
 
@@ -28,12 +64,22 @@ if __name__ == '__main__':
     __path_for_flat = os.path.join(__pathBase, "flat")
 
     photos = [
-        (os.path.join(__path_for_flat, "clear_flat_1.png"), os.path.join(__path_for_raw_report, "dirty_flat_1.png")),
-        (os.path.join(__path_for_flat, "clear_flat_2.png"), os.path.join(__path_for_raw_report, "dirty_flat_2.png")),
-        (os.path.join(__path_for_flat, "clear_flat_3.png"), os.path.join(__path_for_raw_report, "dirty_flat_3.png")),
+        # (os.path.join(__path_for_flat, "clear_flat_1.png"), os.path.join(__path_for_raw_report, "dirty_flat_1.png")),
+        # (os.path.join(__path_for_flat, "clear_flat_2.png"), os.path.join(__path_for_raw_report, "dirty_flat_2.png")),
+        # (os.path.join(__path_for_flat, "clear_flat_3.png"), os.path.join(__path_for_raw_report, "dirty_flat_3.png")),
+        # (os.path.join(__path_for_flat, "clear_1.png"), os.path.join(__path_for_raw_report, "dirty_1.png")),
+        # (os.path.join(__path_for_flat, "clear_2.png"), os.path.join(__path_for_raw_report, "dirty_2.png")),
+        # (os.path.join(__path_for_flat, "clear_3.png"), os.path.join(__path_for_raw_report, "dirty_3.png")),
+        # (os.path.join(__path_for_flat, "clear_4.png"), os.path.join(__path_for_raw_report, "dirty_4.png")),
+        # (os.path.join(__path_for_flat, "clear_5.png"), os.path.join(__path_for_raw_report, "dirty_5.png")),
+        # (os.path.join(__path_for_flat, "clear_1.png"), os.path.join(__path_for_flat, "clear_1.png")),
+        # (os.path.join(__path_for_flat, "clear_2.png"), os.path.join(__path_for_flat, "clear_2.png")),
+        # (os.path.join(__path_for_flat, "clear_3.png"), os.path.join(__path_for_flat, "clear_3.png")),
+        # (os.path.join(__path_for_flat, "clear_4.png"), os.path.join(__path_for_flat, "clear_4.png")),
+        # (os.path.join(__path_for_flat, "clear_5.png"), os.path.join(__path_for_flat, "clear_5.png")),
     ]
     for c, d in photos:
-        s = image(c, d)
+        s = create_image(c, d)
         # clear = Image.open(c)
         # dirty = Image.open(d)
         #
