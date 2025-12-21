4029e6975b45a44e708cca76efbd4b9726b2a1ee
artme <artme@imby.energy>
2025-12-21 21:06:05 +0300

    update v1.4.1

--- tasks/ai_handler.py
+++ tasks/ai_handler.py
@@ -12,24 +12,29 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
-def custom_retry(count:int, max_times: int):
-    def decorator(func):
+class CustomRetry:
+
+    __slots__ = ('count', 'max_times')
+
+    def __init__(self, count: int, max_times: int):
+        self.count = count
+        self.max_times = max_times
+
+    def __call__(self, func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             error = None
-            chunk = max_times / count
-            for i in range(count):
+            chunk = self.max_times / self.count
+            for i in range(self.count):
                 try:
                     return func(*args, **kwargs)
                 except Exception as e:
                     error = e
                     LOGGER.error(f"{type(e).__name__}: {e}")
-                    times = (i+1) * chunk
+                    times = (i + 1) * chunk
                     time.sleep(times)
             raise requests.RequestException(f"{type(error).__name__}: {error}")
-            #return "не удалось чётко распознать фотографию, все минусы выделены на фото"
         return wrapper
-    return decorator
 
 def encoding_file(file: str)-> base64.b64encode:
     with open(file, "rb") as f:
@@ -60,7 +65,7 @@ models = (
 )
 
 
-@custom_retry(count=5, max_times=60)
+@CustomRetry(count=5, max_times=60)
 def create_comment(s: requests.Session, clear: str, dirty: str):
 
     clear_image: base64.b64encode = encoding_file(clear)
